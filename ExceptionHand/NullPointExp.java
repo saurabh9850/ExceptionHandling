6edbd8a604d3ac7322c00a689c55f1532a07b9d3
saurabh9850 <saurabhchavan9850@gmail.com>
2025-06-18 23:58:31 +0530

    Update NullPointExp.java

--- ExceptionHand/NullPointExp.java
+++ ExceptionHand/NullPointExp.java
@@ -1,5 +1,17 @@
 package ExceptionHand;
 
 public class NullPointExp {
+  public static void main(String[] args) {
+        String str = null;
+
+        try {
+            System.out.println("String length: " + str.length());
+        } catch (NullPointerException e) {
+            System.out.println("Exception caught: " + e);
+            System.out.println("You tried to access a method on a null object!");
+        }
+
+        System.out.println("our next code can run hence proved");
+    }
 
 }
